58a631b40319481a4d315da29741097cdb54ea60
Alex Thomassen <alex@thomassen.xyz>
2016-08-11 23:51:55 +0200

    Add base files

---
+++ app/User.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    /**
+     * Indicates the column that is the primary key
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * Indicates if the primary key is auto-incrementing.
+     *
+     * @var bool
+     */
+    protected $incrementing = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id', 'username', 'display_name', 'admin'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'remember_token',
+    ];
+
+    /**
+     * Return user if exists; create and return if doesn't
+     *
+     * @param $user
+     * @return App\User
+     */
+    public static function findOrCreateUser($user)
+    {
+        if ($auth = User::where(['id' => $user->id])->first()) {
+            return $auth;
+        }
+
+        return User::create([
+            'id' => $user->id,
+            'username' => $user->name,
+            'display_name' => $user->nickname
+        ]);
+    }
+}
---
+++ database/migrations/2016_08_10_182428_create_events_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 96);
+            $table->text('description');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('events');
+    }
+}
